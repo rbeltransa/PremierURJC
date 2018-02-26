8f03d8593effe7ae3285d201d6be290fe80aa3aa
fsanchezgar <35699681+fsanchezgar@users.noreply.github.com>
2018-02-26 10:42:18 +0100

    Update RepositorioEntrenador.java

--- PremierURJC/src/main/java/com/example/demo/RepositorioEntrenador.java
+++ PremierURJC/src/main/java/com/example/demo/RepositorioEntrenador.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface RepositorioEntrenador extends JpaRepository<Entrenador, Long> {
 
 	Entrenador findByUserAndPassword(String user, String password);
-
+	Entrenador findByUser(String user);
 }
